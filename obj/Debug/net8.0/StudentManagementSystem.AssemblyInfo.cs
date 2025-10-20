4d34f9c5fd7ffe76eb151c83268c7a62d8a705d3
Bacolod <jpbacolod@HDMF.GOV>
2025-10-20 16:52:28 +0800

    Added login background, Change logout color to red.

--- obj/Debug/net8.0/StudentManagementSystem.AssemblyInfo.cs
+++ obj/Debug/net8.0/StudentManagementSystem.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudentManagementSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a8208bc2ed844f452efae086ff6494f245aaa0e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7db9d423af54dc1d89f7eea23273c25aeabf816")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudentManagementSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StudentManagementSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
